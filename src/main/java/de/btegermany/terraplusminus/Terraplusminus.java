9f113a7d119ee0777029dca1ed50ec0ecd509ddd
Leander <leander.schoe@gmail.com>
2022-05-30 12:30:16 +0200

    Set default config of generateTrees to "false" as the tree generator does not work very well yet

--- src/main/java/de/btegermany/terraplusminus/Terraplusminus.java
+++ src/main/java/de/btegermany/terraplusminus/Terraplusminus.java
@@ -1,8 +1,21 @@
 package de.btegermany.terraplusminus;
 
+import com.comphenix.protocol.PacketType;
+import com.comphenix.protocol.ProtocolLibrary;
+import com.comphenix.protocol.ProtocolManager;
+import com.comphenix.protocol.events.ListenerPriority;
+import com.comphenix.protocol.events.PacketAdapter;
+import com.comphenix.protocol.events.PacketContainer;
+import com.comphenix.protocol.events.PacketEvent;
+import com.comphenix.protocol.reflect.StructureModifier;
 import de.btegermany.terraplusminus.commands.TpllCommand;
+import de.btegermany.terraplusminus.events.PlayerBlockPlacingEvent;
 import de.btegermany.terraplusminus.gen.*;
 import de.btegermany.terraplusminus.utils.FileBuilder;
+import io.netty.buffer.ByteBuf;
+import net.minecraft.network.PacketDataSerializer;
+import net.minecraft.network.protocol.game.ClientboundLevelChunkPacketData;
+import net.minecraft.world.level.chunk.ChunkSection;
 import org.bukkit.Bukkit;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
@@ -11,6 +24,9 @@ import org.bukkit.generator.ChunkGenerator;
 import org.bukkit.plugin.java.JavaPlugin;
 
 import java.lang.reflect.Field;
+import java.lang.reflect.Method;
+import java.nio.ByteBuffer;
+import java.util.Arrays;
 import java.util.Objects;
 import java.util.logging.Level;
 
@@ -44,6 +60,7 @@ public final class Terraplusminus extends JavaPlugin implements Listener {
 
 
         Bukkit.getPluginManager().registerEvents(this,this);
+       // Bukkit.getPluginManager().registerEvents(new PlayerBlockPlacingEvent(), this);
 
         Objects.requireNonNull(getCommand("tpll")).setExecutor(new TpllCommand());
 
@@ -53,9 +70,70 @@ public final class Terraplusminus extends JavaPlugin implements Listener {
                 .addDefault("min-height", -64)
                 .addDefault("max-height", 2032)
                 .addDefault("useBiomes", true)
-                .addDefault("generateTrees",true)
+                .addDefault("generateTrees",false)
                 .copyDefaults(true).save();
 
+     /*   ProtocolManager protocolManager = ProtocolLibrary.getProtocolManager();
+        protocolManager.addPacketListener(
+                new PacketAdapter(this, ListenerPriority.NORMAL,
+                        PacketType.Play.Server.MAP_CHUNK) {
+
+
+                    @Override
+                    public void onPacketSending(PacketEvent event) {
+
+                        if (event.getPacketID() == 0x22) { //event.getPacketID() == 0x22
+                            PacketContainer packet = event.getPacket();
+
+                            ClientboundLevelChunkPacketData oldChunkData = (ClientboundLevelChunkPacketData) packet.getModifier().read(2);
+                            byte[] chunkSections = oldChunkData.a().b();
+
+                            System.out.println("ByteArray: "+ Arrays.toString(chunkSections));
+
+                            byte[] newChunkSections = new byte[3];
+
+                            newChunkSections[0] = chunkSections[chunkSections.length-3];
+                            newChunkSections[1] = chunkSections[chunkSections.length-2];
+                            newChunkSections[2] = chunkSections[chunkSections.length-1];
+
+                            PacketDataSerializer updatedData = new PacketDataSerializer(null);
+
+
+                            updatedData.writeNbt(oldChunkData.getHeightmaps());
+
+                            updatedData.writeInt(newChunkSections.length);
+                            updatedData.writeBytes(newChunkSections);
+
+                            ClientboundLevelChunkPacketData newChunkData = new ClientboundLevelChunkPacketData(updatedData, i , j); //I have no clue about this integers, I'll investigate a bit but not now
+                            event.getPacket().getModifier().write(2,newChunkData);
+
+                            try {
+                                event.getPacket().getModifier().write(2, newPacketDataSerializer);
+                                event.getPlayer().sendMessage("Changed chunk!");
+                            }catch(Exception e){
+                                e.printStackTrace();
+                            }
+
+
+                     }
+                    }
+                });
+        protocolManager.addPacketListener(
+                new PacketAdapter(this, ListenerPriority.NORMAL,
+                        PacketType.Play.Client.POSITION) {
+                    @Override
+                    public void onPacketReceiving(PacketEvent event) {
+
+                        if(event.getPacketType() == PacketType.Play.Client.POSITION){
+                            PacketContainer packet = event.getPacket();
+                            double height = packet.getDoubles().read(1);
+                            //System.out.println("Höhe: "+height);
+
+
+                        }
+                    }
+                });
+*/
         if(Terraplusminus.config.getBoolean("nms")) {
             try {
                 injector = new NMSInjector();
