013654c2e21c8789702a36862b34db354d575e22
Leander <leander.schoe@gmail.com>
2023-03-12 23:41:43 +0100

    fix auto datapack installation when height-datapack=true in config.yml

--- src/main/java/de/btegermany/terraplusminus/Terraplusminus.java
+++ src/main/java/de/btegermany/terraplusminus/Terraplusminus.java
@@ -16,39 +16,23 @@ import org.bukkit.generator.ChunkGenerator;
 
 import org.bukkit.plugin.java.JavaPlugin;
 
-import java.lang.reflect.Field;
-import java.util.Objects;
+import java.io.*;
 import java.util.logging.Level;
 
 
 
 public final class Terraplusminus extends JavaPlugin implements Listener {
-
-    public static final PrivateFieldHandler privateFieldHandler;
     public static FileBuilder config;
-
-    static {
-        PrivateFieldHandler handler;
-        try {
-            Field.class.getDeclaredField("modifiers");
-            handler = new Pre14PrivateFieldHandler();
-        } catch (NoSuchFieldException | SecurityException ex) {
-            handler = new Post14PrivateFieldHandler();
-        }
-        privateFieldHandler = handler;
-    }
 
     @Override
     public void onEnable() {
 
-        Bukkit.getLogger().log(Level.INFO, "\n╭━━━━╮\n" +
+        Bukkit.getLogger().log(Level.INFO,"\n╭━━━━╮\n" +
                 "┃╭╮╭╮┃\n" +
                 "╰╯┃┃┣┻━┳━┳━┳━━╮╭╮\n" +
                 "╱╱┃┃┃┃━┫╭┫╭┫╭╮┣╯╰┳━━╮\n" +
                 "╱╱┃┃┃┃━┫┃┃┃┃╭╮┣╮╭┻━━╯\n" +
                 "╱╱╰╯╰━━┻╯╰╯╰╯╰╯╰╯");
-
-
         Bukkit.getPluginManager().registerEvents(this, this);
 
         getCommand("where").setExecutor(new WhereCommand());
@@ -58,7 +42,6 @@ public final class Terraplusminus extends JavaPlugin implements Listener {
         config = new FileBuilder("plugins/TerraPlusMinus", "config.yml")
                 .addDefault("prefix", "§2§lT+- §8» ")
                 .addDefault("height-datapack", "false")
-                .addDefault("nms", "false")
                 .addDefault("min-height", -64)
                 .addDefault("max-height", 2032)
                 .addDefault("useBiomes", true)
@@ -75,8 +58,6 @@ public final class Terraplusminus extends JavaPlugin implements Listener {
             Bukkit.getPluginManager().registerEvents(new PlayerMoveEvent(this), this);
          }
 
-
-
         Bukkit.getLogger().log(Level.INFO, "[T+-] Plugin loaded");
 
     }
@@ -86,64 +67,47 @@ public final class Terraplusminus extends JavaPlugin implements Listener {
         Bukkit.getLogger().log(Level.INFO, "[T+-] Plugin deactivated");
     }
 
-
-
     @EventHandler
-    public void onWorldInit(WorldInitEvent event) {
-        if(Terraplusminus.config.getBoolean("nms")){
-            if (event.getWorld().getGenerator() instanceof RealWorldGenerator) {
-                NMSInjector injector = null;
-                try {
-                    injector = new NMSInjector();
-                } catch (IllegalArgumentException | SecurityException e) {
-                    e.printStackTrace();
+    public void onWorldInit(WorldInitEvent event)  {
+        String datapackName = "world-height-datapack.zip";
+        File datapackPath = new File(event.getWorld().getWorldFolder() + File.separator + "datapacks" + File.separator + datapackName);
+        if(Terraplusminus.config.getBoolean("height-datapack")) {
+            if (!event.getWorld().getName().contains("_nether") && !event.getWorld().getName().contains("_the_end") ) { //event.getWorld().getGenerator() is null here
+                if(!datapackPath.exists()) {
+                    InputStream in = getResource(datapackName);
+                    OutputStream out;
+                    try {
+                        out = new FileOutputStream(datapackPath);
+                    } catch (FileNotFoundException e) {
+                        Bukkit.getLogger().log(Level.WARNING, "Datapack filepath not found");
+                        throw new RuntimeException(e);
+                    }
+                    byte[] buf = new byte[1024];
+                    int len;
+                    try {
+                        while ((len = in.read(buf)) > 0) {
+                            out.write(buf, 0, len);
+                        }
+                    } catch (IOException io) {
+                        Bukkit.getLogger().log(Level.WARNING, "Could not save " + datapackName);
+                    } finally {
+                        try {
+                            out.close();
+                            Bukkit.getLogger().log(Level.CONFIG, "Copied datapack to world folder");
+                            Bukkit.getLogger().log(Level.INFO, "Stopping server to start again with datapack");
+                            Bukkit.getServer().shutdown();
+                        } catch (IOException e) {
+                            e.printStackTrace();
+                        }
+                    }
                 }
-                injector.attemptInject(event.getWorld());
-                Bukkit.getLogger().log(Level.INFO,"[T+-] §4Activated height expansion");
             }
         }
-        if(Terraplusminus.config.getBoolean("height-datapack")) {
-/*
-            if (event.getWorld().getGenerator().toString().equalsIgnoreCase("TerraPlusMinus")) {
-
-                String datapackPath = event.getWorld().getWorldFolder() + File.separator + "datapacks" + File.separator;
-                System.out.println(datapackPath);
-
-                InputStream inputStream = getResource("world-height-datapack.zip");
-                OutputStream out = new FileOutputStream(file);
-                byte[] buf = new byte[1024];
-                int len;
-                try {
-                    while ((len = in.read(buf)) > 0) {
-                        out.write(buf, 0, len);
-                    }
-                } catch (IOException io) {
-                    System.err.println("[TARDIS] Checker: Could not save the file (" + file + ").");
-                } finally {
-                    try {
-                        out.close();
-                    } catch (IOException e) {
-                        System.err.println("[TARDIS] Checker: Could not close the output stream.");
-                    }
-                }
-            } catch(FileNotFoundException e){
-                System.err.println("[TARDIS] Checker: File not found: " + filename);
-            } */
-        }
-
-
-
     }
 
     @Override
     public ChunkGenerator getDefaultWorldGenerator(String worldName, String id){
-        if(Bukkit.getBukkitVersion().equalsIgnoreCase("1.17.1-R0.1-SNAPSHOT")){
-            Bukkit.getLogger().log(Level.WARNING,"[T+-] §cWorld generation does not work in 1.17");
-            return new RealWorldGenerator(this);
-        }else {
-            return new RealWorldGenerator(this);
-        }
-
+        return new RealWorldGenerator(this);
     }
 
 }
