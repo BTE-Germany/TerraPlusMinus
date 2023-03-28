4cb1251813110000f4ddbd108cd8ce22571ab621
Nudesuppe42 <jufo.theo@gmail.com>
2023-03-28 21:03:16 +0200

    feat(tpll): Height in TPLL Command

--- src/main/java/de/btegermany/terraplusminus/commands/TpllCommand.java
+++ src/main/java/de/btegermany/terraplusminus/commands/TpllCommand.java
@@ -9,7 +9,6 @@ import org.bukkit.Location;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
-import org.bukkit.configuration.file.FileConfiguration;
 import org.bukkit.entity.Player;
 import org.jetbrains.annotations.NotNull;
 
@@ -19,17 +18,17 @@ public class TpllCommand implements CommandExecutor {
 
     @Override
     public boolean onCommand(@NotNull CommandSender commandSender, @NotNull Command command, @NotNull String s, @NotNull String[] args) {
-        if (command.getName().equalsIgnoreCase("tpll")){
+        if (command.getName().equalsIgnoreCase("tpll")) {
             //If sender is not a player cancel the command.
             if (!(commandSender instanceof Player)) {
                 commandSender.sendMessage("This command can only be used by players!");
                 return true;
             }
             Player player = (Player) commandSender;
-            
+
             //Option to passthrough tpll to other bukkit plugins.
             String passthroughTpll = Terraplusminus.config.getString("passthrough_tpll");
-            if (passthroughTpll!=null) {
+            if (passthroughTpll != null) {
                 //Check if any args are parsed.
                 if (args.length == 0) {
                     player.chat("/" + passthroughTpll + ":tpll");
@@ -38,9 +37,9 @@ public class TpllCommand implements CommandExecutor {
                 }
                 return true;
             }
-                                
+
             // -
-            if (args.length == 2) {
+            if (args.length >= 2) {
                 if (player.hasPermission("t+-.tpll")) {
 
                     int move = Terraplusminus.config.getInt("terrain_offset");
@@ -60,37 +59,52 @@ public class TpllCommand implements CommandExecutor {
                         e.printStackTrace();
                     }
 
-                    if(minLat != 0 && maxLat != 0 && minLon != 0 && maxLon != 0){
-                        if(coordinates[1] < minLat || coordinates[0] < minLon || coordinates[1] > maxLat || coordinates[0] > maxLon){
+                    if (minLat != 0 && maxLat != 0 && minLon != 0 && maxLon != 0) {
+                        if (coordinates[1] < minLat || coordinates[0] < minLon || coordinates[1] > maxLat || coordinates[0] > maxLon) {
                             player.sendMessage(Terraplusminus.config.getString("prefix") + "§cYou cannot tpll to these coordinates, because this area is being worked on by another build team.");
                             return true;
                         }
                     }
 
                     TerraConnector terraConnector = new TerraConnector();
-                    double height = terraConnector.getHeight((int) mcCoordinates[0], (int) mcCoordinates[1]).join()+move;
 
-                    if(height > player.getWorld().getMaxHeight()){
+                    double height;
+                    if (args.length >= 3) {
+                        height = Double.parseDouble(args[2]) + move;
+                    } else {
+                        height = terraConnector.getHeight((int) mcCoordinates[0], (int) mcCoordinates[1]).join() + move;
+                    }
+
+                    if (height > player.getWorld().getMaxHeight()) {
                         player.sendMessage(Terraplusminus.config.getString("prefix") + "§cYou cannot tpll to these coordinates, because the world is not high enough at the moment.");
                         return true;
                     }
 
-                    Location location = new Location(player.getWorld(), mcCoordinates[0], height, mcCoordinates[1],player.getLocation().getYaw(), player.getLocation().getPitch());
+                    Location location = new Location(player.getWorld(), mcCoordinates[0], height, mcCoordinates[1], player.getLocation().getYaw(), player.getLocation().getPitch());
+                    player.sendMessage(String.valueOf(location.getY()));
 
-                    if(PaperLib.isChunkGenerated(location)){
-                        location = new Location(player.getWorld(), mcCoordinates[0], player.getWorld().getHighestBlockYAt((int) mcCoordinates[0], (int) mcCoordinates[1])+1, mcCoordinates[1], player.getLocation().getYaw(), player.getLocation().getPitch());
-                    }else{
+                    if (PaperLib.isChunkGenerated(location)) {
+                        if (args.length >= 3) {
+                            location = new Location(player.getWorld(), mcCoordinates[0], height, mcCoordinates[1], player.getLocation().getYaw(), player.getLocation().getPitch());
+                        } else {
+                            location = new Location(player.getWorld(), mcCoordinates[0], player.getWorld().getHighestBlockYAt((int) mcCoordinates[0], (int) mcCoordinates[1]) + 1, mcCoordinates[1], player.getLocation().getYaw(), player.getLocation().getPitch());
+                        }
+                    } else {
                         player.sendMessage(Terraplusminus.config.getString("prefix") + "§7Location is generating. Please wait a moment...");
                     }
-                    PaperLib.teleportAsync(player,location);
+                    PaperLib.teleportAsync(player, location);
                     player.sendMessage(Terraplusminus.config.getString("prefix") + "§7Teleported to " + coordinates[1] + ", " + coordinates[0] + ".");
+
+                    if (args.length >= 3) {
+                        player.sendMessage(Terraplusminus.config.getString("prefix") + "§7Using custom height " + height);
+                    }
 
                     return true;
                 } else {
                     player.sendMessage(Terraplusminus.config.getString("prefix") + "§7No permission for /tpll");
                     return true;
                 }
-            }else {
+            } else {
                 player.sendMessage(Terraplusminus.config.getString("prefix") + "§7Usage: /tpll <longitudes> <latitudes>");
                 return true;
             }
